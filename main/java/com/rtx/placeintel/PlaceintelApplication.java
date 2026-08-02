ae641e6955eab077dddddab8a76009a4f31af23b
Ritesh <riteshchavan5297@gmail.com>
2026-08-02 18:39:07 +0530

    First setup

---
+++ main/java/com/rtx/placeintel/PlaceintelApplication.java
@@ -0,0 +1,13 @@
+package com.rtx.placeintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceintelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceintelApplication.class, args);
+	}
+
+}
---
+++ test/java/com/rtx/placeintel/PlaceintelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtx.placeintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceintelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
